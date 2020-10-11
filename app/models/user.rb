fcfe0d9f88e6ff216fa73a41f24cbf1b3704a0fd
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2020-10-10 23:31:06 -0400

    Add validation to User#pivotal_token
    Also, this commit corrects spacing in relationship methods block (was one space)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,22 @@
 class User < ApplicationRecord
- has_many :pull_requests_assignees
- has_many :pull_requests, through: :pull_requests_assignees
+  has_many :pull_requests_assignees
+  has_many :pull_requests, through: :pull_requests_assignees
 
- has_many :pull_requests_reviewers
- has_many :prs, through: :pull_requests_reviewers, class_name: "PullRequest", source: :pull_request
+  has_many :pull_requests_reviewers
+  has_many :prs, through: :pull_requests_reviewers, class_name: "PullRequest", source: :pull_request
 
- has_many :issues_assignees
- has_many :issues, through: :issues_assignees
+  has_many :issues_assignees
+  has_many :issues, through: :issues_assignees
 
- has_many :pivotal_stories_owners
- has_many :pivotal_stories, through: :pivotal_stories_owners
+  has_many :pivotal_stories_owners
+  has_many :pivotal_stories, through: :pivotal_stories_owners
 
- encrypts :pivotal_token
+  encrypts :pivotal_token
+
+  validates :pivotal_token, length: { is: 32 },
+                            allow_blank: true,
+                            allow_nil: true,
+                            on: :update
 
 
   def self.create_with_omniauth(auth)
