123dcbf6aaa549b0387c0062ba5e81efc82834f0
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2020-10-10 23:34:22 -0400

    Make sure that we are using permitted parameters and are validating input
    We want to make sure that users are submitting pivotal_tokens with 32 characters

--- app/controllers/pivotal_controller.rb
+++ app/controllers/pivotal_controller.rb
@@ -1,12 +1,22 @@
 class PivotalController < ApplicationController
-  def new
+  def edit
     @user = current_user
   end
 
-  def create
-    current_user.update(pivotal_token: params[:pivotal_token])
-    redirect_to todos_path, :flash => {
-      :notice => "Your pivotal tracker id was saved!"
-    }
+  def update
+    if current_user.update(pivotal_token_params)
+      redirect_to todos_path, :flash => {
+        :notice => "Your pivotal tracker id was updated!"
+      }
+    else
+      flash[:error] = current_user.errors.full_messages.first
+      render :edit
+    end
+  end
+
+  private
+
+  def pivotal_token_params
+    params.require(:user).permit(:pivotal_token)
   end
 end
